207adb1fa066371f16d81512e420437a19392228
camball <67163069+camball@users.noreply.github.com>
2025-06-12 04:23:24 -0700

    feat: initial site implementation

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,41 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<svelte:head>
+    <title>dinnerparty.js</title>
+    <meta name="description" content="dinnerparty.js - come eat with us" />
+</svelte:head>
+
+<div class="flex h-[100svh] w-screen flex-col bg-[#F2E7E3] lg:p-6">
+    <div class="relative flex flex-1 flex-col lg:rounded-sm lg:border-2 lg:border-black">
+        <header class="absolute top-0 z-50 w-full">
+            <div class="flex items-center justify-between p-4 lg:px-6 lg:py-4">
+                <div class="h-18 w-18 lg:h-20 lg:w-20">
+                    <img
+                        src="/dinnerparty.js-small.svg"
+                        alt="dinnerparty.js logo"
+                        class="h-full w-full object-contain"
+                    />
+                </div>
+                <a
+                    href="https://instagram.com/dinnerparty.js"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    class="h-8 w-8 lg:h-10 lg:w-10"
+                    aria-label="Instagram"
+                >
+                    <img
+                        src="/black-instagram-icon.svg"
+                        alt="Instagram"
+                        class="h-full w-full object-contain"
+                    />
+                </a>
+            </div>
+        </header>
+        <main class="flex flex-1 flex-col items-center justify-center">
+            <img src="/dinnerparty.js-long.svg" alt="dinnerparty.js" class="h-48 lg:h-64" />
+
+            <script
+                src="https://eocampaign1.com/form/511e7b36-45f7-11f0-8928-d5da82501784.js"
+                data-form="511e7b36-45f7-11f0-8928-d5da82501784"
+            ></script>
+        </main>
+    </div>
+</div>
